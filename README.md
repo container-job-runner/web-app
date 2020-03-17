bc8cd8fbaaf269b8c54046cf05eef52093d1b36a
buvoli <buvoli@uw.edu>
2020-03-17 14:18:08 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jupyter-app
 An simple electron app that can be used to run Jupiter as a separate application
+
+## Building (Mac and Linux)
+
+Clone repository and run the following command in the repository directory:
+
+```bash
+npm install
+npm run package-mac
+npm run package-linux
+```
---
+++ main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(process.env['JURL'] || "")
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
