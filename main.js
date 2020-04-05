a4ec18dbfc9a4163d0b082300c5067b6a9425fb7
buvoli <buvoli@uw.edu>
2020-04-05 03:09:12 -0700

    changes: (1) win is now local, (2) caches are automatically cleared on open, (3) app forcibly exits if app.quit() fails

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-const {app, BrowserWindow} = require('electron')
+const {app, session, BrowserWindow} = require('electron')
 const os = require('os')
 const path = require('path')
 const url = require('url')
 
-let win
-
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 800, center: true})
+  let win = new BrowserWindow({width: 1200, height: 800, center: true})
   // and load the index.html of the app.
   win.loadURL(process.env['URL'] || `file://${path.join(__dirname, "index.html")}`)
-  changeIcon(process.env['ICON'])
-  // Emitted when the window is closed.
-  win.on('closed', () => {win = null})
+  changeIcon(win, process.env['ICON'])
 }
 
-function changeIcon(app_name)
+function changeIcon(window, app_name)
 {
   var switchF, ext
   switch(os.platform())
@@ -24,21 +20,19 @@ function changeIcon(app_name)
       switchF = app.dock.setIcon
       break;
     default:
-      switchF = win.setIcon
+      switchF = window.setIcon
   }
 
   if(app_name === "jupyter" || app_name == "theia")
     switchF(path.join(__dirname, `assets/${app_name}.png`));
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  app.quit()
+app.on('ready', async () => {
+  await session.defaultSession.clearStorageData();
+  createWindow();
 })
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+app.on('window-all-closed', () => {
+  app.quit();
+  setInterval(() => app.exit(1), 3000); // force exit after 3 seconds
 })
