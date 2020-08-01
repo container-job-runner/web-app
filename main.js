bad3c6bdd295ebaaa7be6976c226e421f894a0fb
Tommaso Buvoli <buvoli@Tommasos-MBP.hsd1.ca.comcast.net>
2020-07-31 17:35:33 -0700

    removed unecessary logic for window-all-closed event

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const {app, session, BrowserWindow} = require('electron')
+const { app, session, BrowserWindow } = require('electron')
 const os = require('os')
 const path = require('path')
 const url = require('url')
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({width: 1200, height: 800, center: true})
+  let win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    center: true
+  })
   // and load the index.html of the app.
   win.loadURL(process.env['URL'] || `file://${path.join(__dirname, "index.html")}`)
   changeIcon(win, process.env['ICON'])
@@ -34,5 +38,4 @@ app.on('ready', async () => {
 
 app.on('window-all-closed', () => {
   app.quit();
-  setInterval(() => app.exit(1), 3000); // force exit after 3 seconds
 })
